6c88f2a0f6e2bea45f71fcb2bff55485436ae6fa
elizabethkim828 <elizabethkim828@gmail.com>
2016-06-17 02:26:54 -0400

    can post new events

--- www/js/event.factory.js
+++ www/js/event.factory.js
@@ -9,5 +9,17 @@ app.factory('EventFactory', function($http) {
 		})
 	}
 
+	obj.getById = function(id) {
+		return $http.get('/app/events/' + id).then(function(res) {
+			return res.data
+		})	
+	}
+
+	obj.postEvent = function(data) {
+		return $http.post('/app/events/', data).then(function(res) {
+			return res.data
+		})	
+	}
+
 	return obj
 })
