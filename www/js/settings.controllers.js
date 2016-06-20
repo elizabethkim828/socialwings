9e0521f5de82134ed698e9d36a134cec6ba59ed5
elizabethkim828 <elizabethkim828@gmail.com>
2016-06-20 11:32:24 -0400

    added confirm prompt to delete account

--- www/js/settings.controllers.js
+++ www/js/settings.controllers.js
@@ -1,9 +1,23 @@
 'use strict'
 
-app.controller('SettingsCtrl', function($scope, UserFactory, $state) {
+app.controller('SettingsCtrl', function($scope, $ionicActionSheet, $ionicHistory, UserFactory, $state) {
 	$scope.deleteCurrUser = function() {
-		return UserFactory.deleteCurrUser().then(function() {
-			return $state.go('app.events')
-		})
+		$ionicActionSheet.show({
+	      destructiveText: 'Yes, delete my account',
+	      cancelText: 'Cancel',
+	      titleText: 'Are you sure you want to delete your account?',
+	      destructiveButtonClicked: function() {
+	        return $ionicHistory.clearCache().then(function(){
+	          $ionicHistory.nextViewOptions({
+	            disableBack: true
+	          });
+	        }).then(function() {
+	        	return UserFactory.deleteCurrUser()
+	        }).then(function() {
+				return $state.go('app.events')
+			})
+	      }
+	    })
+		
 	}
 })
